124c1382c42cc224a42d1a6e0e2b9b8b7944d120
lismarreiros <lismarreiros@hotmail.com>
2024-11-13 16:03:55 -0300

    344. criando o carrinho de compras (sem solid)

---
+++ src/srp/shopping-cart.ts
@@ -0,0 +1,66 @@
+type CarItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed'
+
+export class ShoppingCart {
+  private readonly _items: CarItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  addItem(item: CarItem): void {
+    this._items.push(item)
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CarItem[]> {
+    return this._items
+  }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2)
+  }
+
+  checkout(): void {
+    if(this.isEmpty()) {
+      console.log('Seu carrinho está vazio')
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.sendMessage('Seu pedido foi recebido');
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada: ', msg)
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo...');
+    this._items.length = 0
+  }
+}
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addItem({ name: 'Camiseta', price: 48.9 })
+shoppingCart.addItem({ name: 'Caderno', price: 20.9 })
+shoppingCart.addItem({ name: 'Lápis', price: 8.9 })
+shoppingCart.removeItem(0);
+console.log(shoppingCart.orderStatus)
+shoppingCart.checkout()
